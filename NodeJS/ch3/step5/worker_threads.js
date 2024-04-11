0af072bfa900f24f32b2f98f0577fbb45d93a2d4
Donghwi Kim <ehdgnl0904@naver.com>
2024-04-12 02:45:23 +0900

    Node.js 멀티 쓰레드 프로그래밍 학습

---
+++ NodeJS/ch3/step5/worker_threads.js
@@ -0,0 +1,16 @@
+const {
+    Worker, isMainThread, parentPort
+} = require('worker_threads');
+
+if (isMainThread) {     // 메인 스레드
+    const worker = new Worker(__filename);
+    worker.on('message', message => console.log('워커로부터', message));
+    worker.on('exit', () => console.log('워커 끝~'));
+    worker.postMessage('ping');
+} else {        // 워커 스레드
+    parentPort.on('message', (value) => {
+        console.log('부모로부터', value);
+        parentPort.postMessage('pong');
+        parentPort.close();
+    });
+}
