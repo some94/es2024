e4278038c9ab95a5887f9370513ea9ea3b65a68b
Donghwi Kim <ehdgnl0904@naver.com>
2024-04-14 21:18:03 +0900

    pipe로 스트림 연결하기 학습

---
+++ NodeJS/ch3/step6/gzip.js
@@ -0,0 +1,7 @@
+const zlib = require('zlib');
+const fs = require('fs');
+
+const readStream = fs.createReadStream('./readme4.txt');
+const zlibStream = zlib.createGzip();
+const writeStream = fs.createWriteStream('./writeme4.txt');
+readStream.pipe(zlibStream).pipe(writeStream);
