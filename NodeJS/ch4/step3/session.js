6433782c0001aa7fb0c2e6581fbc2dd9023e858a
Donghwi Kim <ehdgnl0904@naver.com>
2024-04-20 01:36:11 +0900

    쿠키/세션 오류 코드 수정

--- NodeJS/ch4/step3/session.js
+++ NodeJS/ch4/step3/session.js
@@ -1,21 +1,26 @@
 const http = require('http');
 const fs = require('fs').promises;
-const path = require('path');
+const url = require('url');
+const qs = require('querystring');
 
 const parseCookies = (cookie = '') =>
-    cookie.split(';').map(v => v.split('=')).reduce((acc, [k, v]) => {
-        acc[k.trim()] = decodeURIComponent(v);
-        return acc;
-    }, {});
+    cookie
+        .split(';')
+        .map(v => v.split('='))
+        .reduce((acc, [k, v]) => {
+            acc[k.trim()] = decodeURIComponent(v);
+            return acc;
+        }, {});
 
 const session = {};
 
 http.createServer(async (req, res) => {
     const cookies = parseCookies(req.headers.cookie);
     if (req.url.startsWith('/login')) {
-        const url = new URL(req.url, 'http://localhost:8085');
-        const name = url.searchParams.get('name');
+        const {query} = url.parse(req.url);
+        const {name} = qs.parse(query);
         const expires = new Date();
+
         expires.setMinutes(expires.getMinutes() + 5);
 
         const uniqueInt = Date.now();
@@ -25,15 +30,16 @@ http.createServer(async (req, res) => {
         };
         res.writeHead(302, {
             Location: '/',
-            'Set-Cookie': `session=${uniqueInt}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
+            'Set-Cookie': `session=${uniqueInt}; Expires=${expires.toUTCString()}; HttpOnly; Path=/`,
         });
         res.end();
+        // 세션쿠키가 존재하고, 만료 기간이 지나지 않았다면
     } else if (cookies.session && session[cookies.session].expires > new Date()) {
         res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8' });
         res.end(`${session[cookies.session].name} 님 안녕하세요`);
     } else {
         try {
-            const data = await fs.readFile(path.join(__dirname, 'cookie2.html'));
+            const data = await fs.readFile('./cookie2.html');
             res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8' });
             res.end(data);
         } catch (err) {
