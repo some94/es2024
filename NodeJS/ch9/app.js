a2024766626988f6f9a58268f62abd8ce441410b
Donghwi Kim <ehdgnl0904@naver.com>
2024-04-29 01:55:06 +0900

    index: 주석 추가 / app: DB 연결

--- NodeJS/ch9/app.js
+++ NodeJS/ch9/app.js
@@ -8,13 +8,21 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const pageRouter = require('./routes/page');
+const { sequelize } = require('./models');
 
 const app = express();
 app.set('port', process.env.PORT || 8001);
 app.set('view engine', 'html');
+
 nunjucks.configure('views', {
     express: app,
     watch: true,
+});
+
+sequelize.sync({ force: false }).then(() => {
+    console.log('데이터베이스 연결 성공');
+}).catch((err) => {
+    console.error(err);
 });
 
 app.use(morgan('dev'));
--- NodeJS/ch9/models/index.js
+++ NodeJS/ch9/models/index.js
@@ -12,18 +12,18 @@ const sequelize = new Sequelize(
 db.sequelize = sequelize;
 
 const basename = path.basename(__filename);
-fs.readdirSync(__dirname)
-  .filter(file => {
+fs.readdirSync(__dirname)   // 현재 폴더의 모든 파일을 조회
+  .filter(file => {   // 숨김 파일, index.js, js 확장자가 아닌 파일 필터링
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
-  .forEach(file => {
+  .forEach(file => {    // 해당 파일의 모델을 불러와서 init
     const model = require(path.join(__dirname, file));
     console.log(file, model.name);
     db[model.name] = model;
     model.initiate(sequelize);
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach(modelName => {    // associate 호출
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
