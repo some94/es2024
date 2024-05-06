ad32493f3f476dfe6bb0c71e4f4de49c26d89e21
Donghwi Kim <ehdgnl0904@naver.com>
2024-05-06 22:43:24 +0900

    닉네임 변경 기능 구현하기

--- NodeJS/ch9_plus/controllers/user.js
+++ NodeJS/ch9_plus/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+let { userCache } = require('../passport/userCache');
 
 exports.follow = async (req, res, next) => {
     try {
@@ -28,4 +29,22 @@ exports.unfollow = async (req, res, next) => {
         console.error(err);
         next(err);
     }
-}
+};
+
+exports.updateNick = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ where: { id: req.user.id } });
+        if (user) {
+            await User.update({ nick: req.body.nick }, {
+                where: { id: req.user.id }
+            })
+            userCache[req.user.id].nick = req.body.nick;
+            res.redirect('/');
+        } else {
+            res.status(404).send('No User');
+        }
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+};
--- NodeJS/ch9_plus/routes/user.js
+++ NodeJS/ch9_plus/routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const {isLoggedIn} = require("../middlewares");
-const {follow, unfollow} = require('../controllers/user');
+const {follow, unfollow, updateNick } = require('../controllers/user');
 
 const router = express.Router();
 
 router.post('/:id/follow', isLoggedIn, follow);
-
 router.post('/:id/unfollow', isLoggedIn, unfollow);
+
+router.post('/updateNick', isLoggedIn, updateNick);
 
 module.exports = router;
