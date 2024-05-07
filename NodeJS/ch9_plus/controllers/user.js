73810c12c61b703cfbbb0d3b9325807384914c7f
Donghwi Kim <ehdgnl0904@naver.com>
2024-05-08 01:02:04 +0900

    비밀번호 변경 기능 구현

--- NodeJS/ch9_plus/controllers/user.js
+++ NodeJS/ch9_plus/controllers/user.js
@@ -1,5 +1,10 @@
-const User = require("../models/user");
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 let { userCache } = require('../passport/userCache');
+
+exports.renderPassword = (req, res) => {
+    res.render('password', { title: '비밀번호 변경 - NodeBird Plus'});
+};
 
 exports.follow = async (req, res, next) => {
     try {
@@ -8,7 +13,7 @@ exports.follow = async (req, res, next) => {
             await user.addFollowing(parseInt(req.params.id, 10));
             res.send('success');
         } else {
-            res.status(404).send('NO USER');
+            res.status(404).send('No User');
         }
     } catch (err) {
         console.error(err);
@@ -42,6 +47,30 @@ exports.updateNick = async (req, res, next) => {
             res.redirect('/');
         } else {
             res.status(404).send('No User');
+        }
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+};
+
+exports.updatePassword = async(req, res, next) => {
+    const { currentPassword, newPassword, confirmPassword } = req.body;
+    try {
+        const user = await User.findOne({ where: { id: req.user.id } });
+        if (!user) return res.status(404).send('No User');
+
+        const current = await bcrypt.compare(currentPassword, user.password);
+        if (!current) return res.redirect('/user/update/password?password=current')
+
+        if (newPassword === confirmPassword) {
+            const hash = await bcrypt.hash(newPassword, 12);
+            await User.update({ password: hash }, {
+                where: { id: req.user.id }
+            });
+            return res.redirect('/user/update/password?password=updated');
+        } else {
+            return res.redirect('/user/update/password?password=confirm');
         }
     } catch (err) {
         console.error(err);
--- NodeJS/ch9_plus/routes/user.js
+++ NodeJS/ch9_plus/routes/user.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const {isLoggedIn} = require("../middlewares");
-const {follow, unfollow, updateNick } = require('../controllers/user');
+const {renderPassword, follow, unfollow, updateNick, updatePassword } = require('../controllers/user');
 
 const router = express.Router();
+
+router.get('/update/password', renderPassword);
 
 router.post('/:id/follow', isLoggedIn, follow);
 router.post('/:id/unfollow', isLoggedIn, unfollow);
 
 router.post('/update/nick', isLoggedIn, updateNick);
+router.post('/update/password', isLoggedIn, updatePassword);
 
 module.exports = router;
