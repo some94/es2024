5895bbae8d0995a42f38a3d82f5da6d4d855baf2
Donghwi Kim <ehdgnl0904@naver.com>
2024-06-07 23:47:00 +0900

    유닛 테스트 - 컨트롤러 테스트하기

--- NodeJS/ch11/controllers/user.js
+++ NodeJS/ch11/controllers/user.js
@@ -3,7 +3,7 @@ const User = require('../models/user');
 exports.follow = async (req, res, next) => {
     try {
         const user = await User.findOne({ where: { id: req.user.id } });
-        if (user) {
+        if (user) {  // req.user.id가 followerId, req.params.id가 followingId
             await user.addFollowing(parseInt(req.params.id, 10));
             res.send('success');
         } else {
