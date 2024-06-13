ba25ac1d68b21a04fb5f5f7f7f9310e6155241dd
Donghwi Kim <ehdgnl0904@naver.com>
2024-06-13 23:48:12 +0900

    Socket.IO 사용하기 예제

--- NodeJS/ch12/socket.js
+++ NodeJS/ch12/socket.js
@@ -1,27 +1,28 @@
-const WebSocket = require('ws')
+const SocketIO = require('socket.io');
 
 module.exports = (server) => {
-    const wss = new WebSocket.Server({ server });
-    // 웹 소켓 연결 시
-    wss.on('connection', (ws, req) => {
+    const io = SocketIO(server, { path: '/socket.io '});
+
+    io.on('connection', (socket) => {    // 웹 소켓 연결 시
+        const req = socket.request;
         const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-        console.log('새로운 클라이언트 접속', ip);
-        // 클라이언트로부터 메시지
-        ws.on('message', (msg) => {
-            console.log(msg.toString());
+        console.log('새로운 클라이언트 접속', ip, socket.id, req.ip);
+
+        socket.on('disconnect', () => {     // 연결 종료 시
+            console.log('클라이언트 접속 해제', ip, socket.id);
+            clearInterval(socket.interval);
         });
-        // 에러 시
-        ws.on('error', console.error);
-        // 연결 종료 시
-        ws.on('close', () => {
-            console.log('클라이언트 접속 해제', ip);
-            clearInterval(ws.interval);
+
+        socket.on('error', (err) => {       // 에러 시
+            console.error(err);
         });
-        // 3초마다 클라이언트로 메시지 전송
-        ws.interval = setInterval(() => {
-            if (ws.readyState === ws.OPEN) {
-                ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
-            }
+
+        socket.on('reply', (data) => {      // 클라이언트로부터 메시지
+            console.log(data);
+        });
+
+        socket.interval = setInterval(() => {       // 3초마다 클라이언트로 메시지 전송
+            socket.emit('news', 'Hello Socket.IO');
         }, 3000);
     });
 };
