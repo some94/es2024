3e1b0f46c7e39e4aa90de6732867c8c6d62d3776
Donghwi Kim <ehdgnl0904@naver.com>
2024-06-15 02:08:27 +0900

    소켓 이벤트 연결하기

--- NodeJS/ch12/socket.js
+++ NodeJS/ch12/socket.js
@@ -1,28 +1,27 @@
 const SocketIO = require('socket.io');
 
-module.exports = (server) => {
+module.exports = (server, app) => {
     const io = SocketIO(server, { path: '/socket.io '});
+    app.set('io', io);
+    const room = io.of('/room');
+    const chat = io.of('/chat');
 
-    io.on('connection', (socket) => {    // 웹 소켓 연결 시
-        const req = socket.request;
-        const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-        console.log('새로운 클라이언트 접속', ip, socket.id, req.ip);
+    room.on('connection', (socket) => {
+        console.log('room 네임스페이스에 접속');
+        socket.on('disconnect', () => {
+            console.log('room 네임스페이스 접속 해제');
+        });
+    });
 
-        socket.on('disconnect', () => {     // 연결 종료 시
-            console.log('클라이언트 접속 해제', ip, socket.id);
-            clearInterval(socket.interval);
+    chat.on('connection', (socket) => {
+        console.log('chat 네임스페이스에 접속');
+
+        socket.on('join', (data) => {       // data는 브라우저에서 보낸 방 아이디
+            socket.join(data);      // 네임스페이스 아래 존재하는 방에 접속
         });
 
-        socket.on('error', (err) => {       // 에러 시
-            console.error(err);
+        socket.on('disconnect', () => {
+            console.log('chat 네임스페이스 접속 해제');
         });
-
-        socket.on('reply', (data) => {      // 클라이언트로부터 메시지
-            console.log(data);
-        });
-
-        socket.interval = setInterval(() => {       // 3초마다 클라이언트로 메시지 전송
-            socket.emit('news', 'Hello Socket.IO');
-        }, 3000);
     });
 };
